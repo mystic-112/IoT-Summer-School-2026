fa7b98eb5ed3933c0dd0bb78d2e6dbb44405bee1
mystic-112 <abhishektandon4111@gmail.com>
2026-07-06 17:44:52 +0530

    Create config.h
    feat: add config.h support for WiFi credentials and API configuration

---
+++ WEEK-4/REST_API_Weather_Data_Fetcher/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// wifi credentials
+#define WIFI_SSID "Abhishek"
+#define WIFI_PASSWORD "11223344"
+
+// openweathermap api url
+#define WEATHER_API_URL "https://api.open-meteo.com/v1/forecast?latitude=32.8083&longitude=74.8958&current=temperature_2m,relative_humidity_2m&timezone=auto"
+
+#endif
