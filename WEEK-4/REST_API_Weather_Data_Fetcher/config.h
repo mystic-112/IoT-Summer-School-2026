5f60c20032cc7792a7bb8be5cdd755f028084674
ABHISHEK TANDON <abhishektandon4111@gmail.com>
2026-07-06 23:38:44 +0530

    Update config.h

--- WEEK-4/REST_API_Weather_Data_Fetcher/config.h
+++ WEEK-4/REST_API_Weather_Data_Fetcher/config.h
@@ -6,6 +6,6 @@
 #define WIFI_PASSWORD "11223344"
 
 // openweathermap api url
-#define WEATHER_API_URL "https://api.open-meteo.com/v1/forecast?latitude=32.8083&longitude=74.8958&current=temperature_2m,relative_humidity_2m&timezone=auto"
+#define WEATHER_API_URL "https://api.open-meteo.com/v1/forecast?latitude=32.8083&longitude=74.8958&current=temperature_2m,relative_humidity_2m"
 
 #endif
